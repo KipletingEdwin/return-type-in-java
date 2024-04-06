8fe97b47c10aeb62eea7c58c8dfa429d489c004c
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-06 22:09:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println(add(10,5));
+
+    }
+
+    static int add(int x, int y){
+        int z = x + y;
+        return z;
+    }
+
+
+}
